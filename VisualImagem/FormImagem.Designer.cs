8c7c9f3799907dadf039a3a4d0b579e53e051e11
RonaldoMagalhaes <ronaldo.magalhaes1@gmail.com>
2023-05-01 19:17:32 -0300

    Add project files.

---
+++ VisualImagem/FormImagem.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace VisualImagem
+{
+    partial class FormImagem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.dglAbrir = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImagem
+            // 
+            this.picImagem.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picImagem.Location = new System.Drawing.Point(12, 12);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(526, 391);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagem.TabIndex = 0;
+            this.picImagem.TabStop = false;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(332, 409);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 47);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "&Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(463, 409);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 47);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // dglAbrir
+            // 
+            this.dglAbrir.Filter = "Arquivos de Imagens |*.*";
+            this.dglAbrir.InitialDirectory = "C:\\Users\\ronal\\Pictures";
+            this.dglAbrir.Title = "Abertura de Imagem";
+            // 
+            // FormImagem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 517);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.picImagem);
+            this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimizeBox = false;
+            this.Name = "FormImagem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.OpenFileDialog dglAbrir;
+    }
+}
+
---
+++ VisualImagem/FormImagem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualImagem
+{
+    public partial class FormImagem : Form
+    {
+        public FormImagem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dglAbrir.ShowDialog();
+                if(dglAbrir.FileName != "")
+                {
+                    picImagem.Load(dglAbrir.FileName);
+                }
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show(erro.Message + "\nO arquivo solicitado não é um arquivo de imagem", "**** ERRO DE ARQUIVO ****", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Deseja encerrar o programa?", "****** FINALIZANDO O PROGRAMA ******", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
